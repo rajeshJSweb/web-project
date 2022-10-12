cd4db6a63f8e5c5e2bbc1babf8e58579f18857cb
Md Rashedul Islam <rashed.rsd4@gmail.com>
2022-10-12 14:27:50 +0600

    route fix

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -20,7 +19,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -29,13 +27,14 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
+  console.log(err)
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  //res.render('error');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Express' });
+});
+
+router.get('/registration', function(req, res, next) {
+  res.render('registration', { title: 'Express' });
+});
+
+router.get('/student-list', function(req, res, next) {
+  res.render('student-list', { title: 'Express' });
+});
+
+router.get('/add-student', function(req, res, next) {
+  res.render('add-student', { title: 'Express' });
+});
+
 module.exports = router;
